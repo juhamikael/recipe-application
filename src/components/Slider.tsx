fbe5003c2d4ca139821bd3223cb7214883c694a2
Juhamikael <contact.juhamusic@gmail.com>
2023-04-08 03:18:03 +0300

    New components for create / show recipes

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,17 +1,50 @@
 import { Slider } from "@mantine/core";
 
 type IProps = {
+  defaultValue?: number;
   label: string;
   steps: number;
-  data: unknown;
+  onChange?: (value: number) => void;
+  data?: unknown;
+  value?: number;
+  max?: number;
+  type?: "time" | "other";
 };
 
 const CustomSlider: React.FC<IProps> = (props) => {
-  const { data, label, steps } = props;
+  const {
+    data,
+    defaultValue = 50,
+    label,
+    steps,
+    onChange,
+    value,
+    max = 100,
+  } = props;
+
   return (
     <div>
-      <div className="my-2">{label}</div>
-      <Slider defaultValue={50} step={steps} marks={data} label={null} />
+      <div className="my-2">
+        {label} {value} {props.type === "time" ? "mins" : ""}
+      </div>
+      {data ? (
+        <Slider
+          defaultValue={defaultValue}
+          step={steps}
+          marks={data}
+          label={null}
+          onChange={onChange}
+          max={max}
+        />
+      ) : (
+        <Slider
+          defaultValue={defaultValue}
+          step={steps}
+          label={null}
+          onChange={onChange}
+          max={max}
+        />
+      )}
     </div>
   );
 };
