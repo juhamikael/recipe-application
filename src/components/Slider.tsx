8c9585be3fa5459464fb23ef9245648c427fbbcf
Juhamikael <contact.juhamusic@gmail.com>
2023-04-13 11:44:12 +0300

    Modify slider component

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,14 +1,16 @@
+import { useEffect, useState } from "react";
 import { Slider } from "@mantine/core";
 
 type IProps = {
   defaultValue?: number;
   label: string;
   steps: number;
-  onChange?: (value: number) => void;
+  onChange?: (value: number | string) => void;
   data?: unknown;
-  value?: number;
+  value?: number | string;
   max?: number;
   type?: "time" | "other";
+  marks?: string[];
 };
 
 const CustomSlider: React.FC<IProps> = (props) => {
@@ -20,29 +22,56 @@ const CustomSlider: React.FC<IProps> = (props) => {
     onChange,
     value,
     max = 100,
+    marks,
   } = props;
+
+  const [sliderValue, setSliderValue] = useState<number>(defaultValue);
+
+  useEffect(() => {
+    if (value !== undefined) {
+      setSliderValue(value);
+    }
+  }, [value]);
+
+  const handleChange = (newValue: number) => {
+    setSliderValue(newValue);
+    if (onChange) {
+      if (marks && marks.length > 0) {
+        onChange(marks[newValue]);
+      } else {
+        onChange(newValue);
+      }
+    }
+  };
+
+  const displayValue = marks ? marks[sliderValue] : sliderValue;
+
+  const sliderMarks = marks
+    ? marks.map((mark, index) => ({ value: index, label: mark }))
+    : undefined;
 
   return (
     <div>
       <div className="my-2">
-        {label} {value} {props.type === "time" ? "mins" : ""}
+        {label} {displayValue} {props.type === "time" ? "mins" : ""}
       </div>
       {data ? (
         <Slider
-          defaultValue={defaultValue}
+          value={sliderValue}
           step={steps}
           marks={data}
           label={null}
-          onChange={onChange}
+          onChange={handleChange}
           max={max}
         />
       ) : (
         <Slider
-          defaultValue={defaultValue}
+          value={sliderValue}
           step={steps}
           label={null}
-          onChange={onChange}
+          onChange={handleChange}
           max={max}
+          marks={sliderMarks}
         />
       )}
     </div>
